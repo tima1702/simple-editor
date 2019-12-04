a6dde3f77ff325dcc12cdc1f8446b389da3a8aa2
Timofey Kuznetsov <tima1702@gmail.com>
2019-12-04 09:24:05 +0300

    Added env port

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.get('/', function(req, res) {
   res.status(200).sendFile(__dirname + '/index.html');
@@ -7,7 +8,7 @@ app.get('/', function(req, res) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
   console.log('App run on 3000');
 });
 
